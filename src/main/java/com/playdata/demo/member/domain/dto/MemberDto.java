c19d175f0bc7ad308ef926ca3456228bc2991f08
ktk8916 <ktk8916@naver.com>
2023-07-11 17:49:04 +0900

    id도 반환

--- src/main/java/com/playdata/demo/member/domain/dto/MemberDto.java
+++ src/main/java/com/playdata/demo/member/domain/dto/MemberDto.java
@@ -3,10 +3,11 @@ package com.playdata.demo.member.domain.dto;
 import com.playdata.demo.member.domain.entity.Member;
 import com.playdata.demo.join.MemberHobby;
 
-public record MemberDto(String name, Integer age) {
+public record MemberDto(Integer id, String name, Integer age) {
     public static MemberDto from(MemberHobby memberHobby){
         Member member = memberHobby.getMember();
         return new MemberDto(
+                member.getId(),
                 member.getName(),
                 member.getAge());
     }
